9697edf1043e946f5cbc326c14d473a3a2cab78c
Brandon Hill <brandonjon26@gmail.com>
2021-05-24 13:55:41 -0500

    enemy list is done

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,51 @@
 // Put your code here
+
+console.log("My Enemies List!");
+console.log("----------------");
+
+const myEnemies = () => {
+    const enemies = [
+        {
+            firstName: "Joshua",
+            lastName: "Flowers",
+            offenses: ["Being a jerk to me in elementary school",
+                "Not being nice to me in elementary school"],
+            isReallyHated: true
+        },
+        {
+            firstName: "Darth",
+            lastName: "Vader",
+            offenses: ["Cut off Luke's hand",
+                "Murdered all those kids",
+                "Unkind management practices"],
+            isReallyHated: false
+        },
+        {
+            firstName: "Betty",
+            lastName: "Rudelady",
+            offenses: ["Phone calls in the theater",
+                "Phone calls on the bus",
+                "Phone calls in line at the grocery store",
+                "Poor conversationalist"],
+            isReallyHated: true
+        },
+        {
+            firstName: "Leon",
+            lastName: "Peck",
+            offenses: ["Keeps giving me a hotplate"],
+            isReallyHated: false
+        }
+    ]
+
+    return enemies
+}
+
+const enemies = myEnemies();
+
+for (enemy of enemies) {
+    if (enemy.isReallyHated) {
+        console.log(`${enemy.firstName} ${enemy.lastName} (really, really dislike!)`);
+    } else {
+        console.log(`${enemy.firstName} ${enemy.lastName}`)
+    }
+}
