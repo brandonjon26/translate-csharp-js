bacffdf23e9aa6fad39438699f15d74d9139aa12
Brandon Hill <brandonjon26@gmail.com>
2021-05-24 14:48:42 -0500

    finished place name beacuse I skipped a step

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -8,3 +8,10 @@ console.log("All Place Names");
 for (name of names) {
     console.log(name);
 }
+
+const thePlaceNames = names.filter(name => name.startsWith("The"));
+
+console.log("'The' Place Names");
+for (placeName of thePlaceNames) {
+    console.log(placeName);
+}
